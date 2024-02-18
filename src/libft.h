1f62f6678fddd3d329b9bafaafec56d6c133cea1
MarkosComK <markoscomks@gmail.com>
2024-02-18 10:49:58 +0000

    created header file

--- src/libft.h
+++ src/libft.h
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/17 15:32:18 by marsoare          #+#    #+#             */
-/*   Updated: 2024/02/18 10:49:41 by marsoare         ###   ########.fr       */
+/*   Created: 2024/02/18 10:46:45 by marsoare          #+#    #+#             */
+/*   Updated: 2024/02/18 10:48:00 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef LIBFT_H
+# define LIBFT_H
+#include <stdlib.h>
+#include <unistd.h>
 
-void	*ft_memset(void *s, int c, size_t n)
-{
-	char	*str;
-
-	str = (char *)s;
-	while(n > 0)
-	{
-		str[n - 1] = c;
-		n--;
-	}
-	return (str);
-}
-/*
-#include <stdio.h>
-int	main(void)
-{
-	char	s[6] = "Markos";
-
-	ft_memset(s+2, '.', 1);
-	printf("after my memset(): %s", s);
-	return (0);
-}
-*/
+#endif
