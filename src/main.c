e0bfe6dafcaa28ac4caff47d2f680b4d8e106e16
MarkosComK <markoscomks@gmail.com>
2024-02-22 13:57:00 +0000

    Norm ok

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 13:54:12 by marsoare          #+#    #+#             */
-/*   Updated: 2024/02/22 13:47:36 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/02/22 13:56:51 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 
 int	main(void)
 {
-	char	str[7] = "Markos";
+	char	str[7];
 	char	dest[7];
 
+	strcpy(str, "Markos");
 	ft_memcpy(dest, str, 5);
-	//memcpy(dest, str, 8);
 	printf("%s\n", dest);
 	return (0);
 }
