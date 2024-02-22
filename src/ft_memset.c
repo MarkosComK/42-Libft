b6538aae5ec4eddba2153398124714a5b843a0e5
MarkosComK <markoscomks@gmail.com>
2024-02-22 13:03:26 +0000

    memset norm ok

--- src/ft_memset.c
+++ src/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 15:32:18 by marsoare          #+#    #+#             */
-/*   Updated: 2024/02/18 11:21:28 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/02/22 13:03:08 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	*ft_memset(void *s, int c, size_t n)
 {
 	char	*str;
 
-	str = (char *)s;
-	while(n > 0)
+	str = (char *) s;
+	while (n > 0)
 	{
 		str[n - 1] = c;
 		n--;
