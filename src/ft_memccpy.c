d02612088f0c544bd6e95c9435963440d3b81f27
MarkosComK <markoscomks@gmail.com>
2024-02-22 14:11:56 +0000

    Norm ok

---
+++ src/ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 13:57:26 by marsoare          #+#    #+#             */
+/*   Updated: 2024/02/22 14:11:35 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+{
+	char	*d;
+	char	*s;
+
+	d = (char *)dest;
+	s = (char *)src;
+	while (n > 0)
+	{
+		*d = *s;
+		if (*s == c)
+			break ;
+		s++;
+		d++;
+		n--;
+	}
+	return (d);
+}
