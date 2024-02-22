1882e1b28be0c6ed0bf6b1add2048e65bd3cd125
MarkosComK <markoscomks@gmail.com>
2024-02-22 13:12:57 +0000

    Norm ok

--- src/libft.h
+++ src/libft.h
@@ -6,14 +6,15 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 10:46:45 by marsoare          #+#    #+#             */
-/*   Updated: 2024/02/20 17:58:20 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/02/22 13:12:38 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-#include <stdlib.h>
-#include <unistd.h>
+
+# include <stdlib.h>
+# include <unistd.h>
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
