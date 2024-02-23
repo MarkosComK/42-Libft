5e5794dccfb369ea320db8f4ad2a3d0717dab8aa
MarkosComK <markoscomks@gmail.com>
2024-02-23 09:47:26 +0000

    ft_memmove working

---
+++ src/ft_memmove.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 14:17:04 by marsoare          #+#    #+#             */
+/*   Updated: 2024/02/23 09:46:14 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*d;
+	char	*s;
+
+	d = (char *)dest;
+	s = (char *)src;
+	if (d + n > s && s > d)
+		ft_memcpy(d, s, n);
+	else
+		while(n--)
+		{
+			*d = *s;
+			d++;
+			s++;
+		}
+	return (d);
+}
