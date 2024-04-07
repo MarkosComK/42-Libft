b8958dd205f0caefef343dfe52875084c0f0c454
MarkosComK <markoscomks@gmail.com>
2024-04-07 12:55:18 +0100

    atoi removing spaces

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 12:24:06 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/07 12:51:34 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/07 12:54:27 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	ft_atoi(const char *ptr)
 {
 	int	result;
 
+	result = 0;
 	while(*ptr == 32)
 	{
 		ptr++;
