8388bd23f496c4ba7ad7e2880d7bf7b4c62b2d06
markoscomks <markoscomks@gmail.com>
2024-04-12 13:19:09 +0100

    added missing functions

--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 10:46:45 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/10 13:33:47 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/12 13:18:55 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_atoi(const char *ptr);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
 
 #endif
