87a3ef80efd8fac66fb3e98d8cf6a3bc051ba7b1
markoscomks <markoscomks@gmail.com>
2024-04-13 13:03:24 +0100

    Norm ok

--- src/ft_memcmp.c
+++ src/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 18:16:39 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/12 18:35:16 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/13 13:03:03 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
-	unsigned char *p1;
-	unsigned char *p2;
+	size_t			i;
+	unsigned char	*p1;
+	unsigned char	*p2;
 
 	i = 0;
 	p1 = (unsigned char *)s1;
