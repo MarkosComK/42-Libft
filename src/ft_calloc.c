05922cd42ef7a38a042b111c74c9cde731e16c15
markoscomks <markoscomks@gmail.com>
2024-04-13 13:02:20 +0100

    Norm ok

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 12:24:27 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/13 12:53:26 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/13 13:02:05 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*p;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(size * nmemb);
-	while(nmemb > 0)
+	while (nmemb > 0)
 	{
 		((char *)p)[nmemb - 1] = 0;
 		nmemb--;
