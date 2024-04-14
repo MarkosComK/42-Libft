2f412f9499d1d26eb0920d009ef76f694164ad8c
markoscomks <markoscomks@gmail.com>
2024-04-14 21:09:06 +0100

    ft_strtrim

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 14:49:43 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/13 15:27:28 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/14 21:08:42 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,27 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	int		i;
+	int		j;
+	int		k;
 	char	*result;
-	int		total;
 
-	total = checkcases(s1, set);
+	i = 0;
+	j = 0;
+	k = 0;
+	while (ft_strchr(set, (int)s1[i]))
+		i++;
+	while (s1[i + j])
+		j++;
+	while (ft_strrchr(set, (int)s1[j]))
+		j--;
+	result = (char *)malloc(sizeof(char) * (j + 1));
+	while (j > 0)
+	{
+		result[k] = s1[i++];
+		k++;
+		j--;
+	}
+	result[k] = 0;
+	return (result);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 14:34:46 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/14 19:09:35 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/14 20:50:29 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
 int	main(int ac, char **av)
 {
 	(void) ac;
-	int	value = ft_atoi(av[1]);
-	char	*result = ft_itoa(value);
-	printf("%s\n", result);
+	(void) av;
+	printf("%s\n", ft_strtrim("**exa*mple**a", "*"));
 	return (0);
 }
