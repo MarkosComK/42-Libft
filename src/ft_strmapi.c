0cd74274338b1a27e286363b2c03c2ad04563865
markoscomks <markoscomks@gmail.com>
2024-04-15 08:41:11 +0100

    Norm ok

--- src/ft_strmapi.c
+++ src/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 07:51:59 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/15 08:14:58 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/15 08:40:53 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*result;
-	size_t		i;
+	size_t	i;
 
 	result = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
-	if (!result)
+	if (!result || !s)
 		return (NULL);
 	i = 0;
-	while(i < ft_strlen(s))
+	while (i < ft_strlen(s))
 	{
 		result[i] = (*f)(i, s[i]);
 		i++;
