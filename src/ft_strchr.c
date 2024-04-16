1675ff8a46b4f6885f2448c94d30aec668124f99
markoscomks <markoscomks@gmail.com>
2024-04-16 13:23:35 +0100

    Added consideration for \0 character

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:07:01 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/16 12:18:18 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/16 13:01:30 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	if (!s || !c)
-		return (NULL);
 	while (*s)
 	{
 		if (*s == (char)c)
 			return ((char *)s);
 		s++;
 	}
+	if ((char)c == *s)
+		return ((char *)s);
 	return (NULL);
 }
