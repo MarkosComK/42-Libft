6f4ac6f494daca3727639e7763f72c372dee0aaf
markoscomks <markoscomks@gmail.com>
2024-04-16 14:09:44 +0100

    added missing files

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 14:34:46 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/16 12:21:15 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/16 14:06:07 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ int	main(int ac, char **av)
 	(void) av;
 	//char	s1[] = "Test";
 	//char	s2[] = "Test";
-	//printf("Lib: %s\n", strchr(NULL, (int)'S'));
-	printf("Mine: %s\n", ft_strchr(NULL, (int)'S'));
-	//printf("Memory b:%s\nMemory c: %s\n", s3, (char *)ft_memcpy(s4, s3, 5));
+	printf("Mine: %s\n", ft_strnstr("", "xx", 0));
 	return (0);
 }
