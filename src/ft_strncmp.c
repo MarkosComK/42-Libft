d71434243a419876305d5170a0f8f3d7d2076f41
markoscomks <markoscomks@gmail.com>
2024-04-16 13:45:18 +0100

    solved issues considering special cases

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:59:14 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/13 13:04:09 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/16 13:44:54 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	j;
 
 	j = 0;
-	while (s1[j] && s2[j] && j < n)
+	while (s1[j] == s2[j] && s1[j] && j < n)
 	{
 		if (s1[j] != s2[j])
-			return (s1[j] - s2[j]);
+			return ((unsigned int char)s1[j] - (unsigned int char)s2[j]);
 		j++;
 	}
+	if (j < n)
+			return ((unsigned int char)s1[j] - (unsigned int char)s2[j]);
 	return (0);
 }
