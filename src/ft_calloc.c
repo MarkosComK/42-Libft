5e540422c442e6a19676d56ea5bc88502bfdeb82
markoscomks <markoscomks@gmail.com>
2024-04-17 13:33:57 +0100

    calloc works with zero size

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 12:24:27 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/17 13:30:45 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/17 13:33:00 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*p;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
 	p = malloc(size * nmemb);
 	if (p)
 		ft_bzero(p, nmemb * size);
