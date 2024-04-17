f93516f7a5a3bc318bea2a628d97433bda76f7da
markoscomks <markoscomks@gmail.com>
2024-04-17 13:27:44 +0100

    calloc bzero usage

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 12:24:27 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/13 13:02:05 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/17 13:27:14 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(size * nmemb);
-	while (nmemb > 0)
-	{
-		((char *)p)[nmemb - 1] = 0;
-		nmemb--;
-	}
+	while (p)
+		ft_bzero(ptr, nmemb * size);
 	return (p);
 }
