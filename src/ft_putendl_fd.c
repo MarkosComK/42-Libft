6921c64734e22e498f59bbf85f33952f4dce4bbc
markoscomks <markoscomks@gmail.com>
2024-04-17 10:28:29 +0100

    Solved issue with fd

--- src/ft_putendl_fd.c
+++ src/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 09:54:40 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/15 09:56:11 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/17 10:28:06 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void	ft_putendl_fd(char *s, int fd)
 {
 	ft_putstr_fd(s, fd);
-	ft_putchar_fd('\n', 1);
+	ft_putchar_fd('\n', fd);
 }
