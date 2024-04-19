f84e123c745a43de7bfe258ead4da9cd4b920eab
markoscomks <markoscomks@gmail.com>
2024-04-19 13:41:15 +0100

    Norm ok

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:59:14 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/16 13:56:56 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/19 13:41:02 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		j++;
 	}
 	if (j < n)
-			return ((unsigned char)s1[j] - (unsigned char)s2[j]);
+		return ((unsigned char)s1[j] - (unsigned char)s2[j]);
 	return (0);
 }
