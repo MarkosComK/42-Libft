809d995acd00661cef469bf563ae4e264715cfb1
markoscomks <markoscomks@gmail.com>
2024-04-20 17:43:46 +0100

    Ident changes + readable

--- src/ft_lstnew.c
+++ src/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 16:04:12 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/20 16:50:32 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/20 17:24:14 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_list	*ft_lstnew(void *content)
 	lstnew = (t_list *)malloc(sizeof(t_list));
 	if (!lstnew)
 		return (NULL);
-	lstnew->content = content;
-	lstnew->next = 0;
+	lstnew -> content = content;
+	lstnew -> next = 0;
 	return (lstnew);
 }
