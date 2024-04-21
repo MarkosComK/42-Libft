a0248165af4dbccb09d644e02966341a3590324c
markoscomks <markoscomks@gmail.com>
2024-04-21 17:45:46 +0100

    Added if alocation fail

--- src/ft_lstmap.c
+++ src/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 15:55:34 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/21 16:55:34 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/21 17:41:27 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,16 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_node;
 	void	*set;
 
-	(void) del;
 	new_list = NULL;
 	while (lst)
 	{
 		set = f(lst -> content);
 		new_node = ft_lstnew(set);
+		if (!new_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&new_list, new_node);
 		lst = lst -> next;
 	}
