4c7b436a541707e4182295bcf9073619110f279a
markoscomks <markoscomks@gmail.com>
2024-04-21 15:12:38 +0100

    Added func

---
+++ src/ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/21 14:09:39 by marsoare          #+#    #+#             */
+/*   Updated: 2024/04/21 15:10:46 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	temp;
+
+	if (!lst)
+		return ;
+	while (*lst)
+	{
+		temp = (*lst) -> next;
+		ft_lstdelone(*lst, del);
+		*lst = temp;
+	}
+	free(*lst);
+}
