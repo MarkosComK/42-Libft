0765b9c5b71659f1b7f704bbac1fc827e4e29a02
markoscomks <markoscomks@gmail.com>
2024-04-21 15:46:20 +0100

    Changed temp to *temp

--- src/ft_lstclear.c
+++ src/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 14:09:39 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/21 15:13:08 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/21 15:45:56 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list	temp;
+	t_list	*temp;
 
 	if (!lst)
 		return ;
