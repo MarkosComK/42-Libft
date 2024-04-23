250d4b6c0e627a0bc3f81102689e89625ce66fbc
markoscomks <markoscomks@gmail.com>
2024-04-23 13:22:50 +0100

    Return param fixed

--- src/ft_lstiter.c
+++ src/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 15:19:01 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/23 13:13:36 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/23 13:22:36 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (!lst)
-		return (NULL);
+		return ;
 	while (lst)
 	{
 		f(lst -> content);
