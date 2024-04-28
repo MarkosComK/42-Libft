456117bb94ca37b4924313c659738f2eda408680
markoscomks <markoscomks@gmail.com>
2024-04-28 17:29:18 +0100

    Added null string case

--- src/ft_putstr.c
+++ src/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 14:17:58 by marsoare          #+#    #+#             */
-/*   Updated: 2024/04/28 14:28:40 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/04/28 17:28:55 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_putstr(char *str)
 	int	i;
 
 	i = 0;
+	if (str == NULL)
+		return (write(1, "(null)", 6));
 	while (str[i])
 		write(1, &str[i++], 1);
 	return (i);
