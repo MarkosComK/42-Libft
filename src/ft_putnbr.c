259a1751bc9349017a1cdcb17fc4ee6e96984c43
markoscomks <markoscomks@gmail.com>
2024-04-28 14:45:15 +0100

    Created func

---
+++ src/ft_putnbr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 14:30:52 by marsoare          #+#    #+#             */
+/*   Updated: 2024/04/28 14:44:19 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_putnbr(int n)
+{
+	long	nb;
+	int		count;
+
+	nb = n;
+	count = 0;
+	if (nb < 0)
+	{
+		count += ft_putchar('-');
+		nb = nb * -1;
+	}
+	if (nb > 9)
+	{
+		count += ft_putnbr(nb / 10);
+		count += ft_putnbr(nb % 10);
+	}
+	else
+		count += ft_putchar(nb + 48);
+	return (count);
+}
