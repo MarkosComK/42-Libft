a6aa2adb6dec234be2b9fc2c19d8e9f3b24169e2
MarkosComK <markoscomks@gmail.com>
2024-10-23 13:40:05 +0100

    Modified ft_strjoin.c

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -22,7 +22,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (ft_strdup(s2));
 	if (!s2)
 		return (ft_strdup(s1));
-	endstr = (char *)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
+	endstr = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (endstr == 0)
 		return (NULL);
 	i = 0;
